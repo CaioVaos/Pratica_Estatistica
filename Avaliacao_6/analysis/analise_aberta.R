bc001cb12fdf4ac4cf1d674ab177c7b20b14eb28
CaioVaos <salvianocaio@gmail.com>
2026-04-24 20:18:15 -0300

    Atualizações AV5 e AV6

--- Avaliacao_6/analysis/analise_aberta.R
+++ Avaliacao_6/analysis/analise_aberta.R
@@ -238,25 +238,75 @@ plot_nuvem_facet
 ## wordcloud2 ------------------------------------------------------------------
 
 ### Unica ----
-frequencia_palavras_2 = frequencia_palavras%>%
-  mutate(n=log(n+1))
-wordcloud2(
+# frequencia_palavras_2 = frequencia_palavras%>%
+#   mutate(n=log(n+1))
+# wordcloud2(
+#   data = frequencia_palavras_2,
+#   size = 0.6,          
+#   fontWeight = 'bold',  
+#   color='random-light', 
+#   backgroundColor="dark",
+#   shape="circle"
+# )
+### Modificado ----
+# https://github.com/Lchiffon/wordcloud2/issues/63
+
+wordcloud3 <- function (data, size = 1, minSize = 0, gridSize = 0, fontFamily = "Segoe UI", 
+                        fontWeight = "bold", color = "random-dark", backgroundColor = "white", 
+                        minRotation = -pi/4, maxRotation = pi/4, shuffle = TRUE, 
+                        rotateRatio = 0.4, shape = "circle", ellipticity = 0.65, 
+                        widgetsize = NULL, figPath = NULL, hoverFunction = NULL) {
+  if ("table" %in% class(data)) {
+    dataOut = data.frame(name = names(data), freq = as.vector(data))
+  }
+  else {
+    data = as.data.frame(data)
+    dataOut = data[, 1:3]
+    names(dataOut) = c("name", "freq", "label")
+  }
+  if (!is.null(figPath)) {
+    if (!file.exists(figPath)) {
+      stop("cannot find fig in the figPath")
+    }
+    spPath = strsplit(figPath, "\\.")[[1]]
+    len = length(spPath)
+    figClass = spPath[len]
+    if (!figClass %in% c("jpeg", "jpg", "png", "bmp", "gif")) {
+      stop("file should be a jpeg, jpg, png, bmp or gif file!")
+    }
+    base64 = base64enc::base64encode(figPath)
+    base64 = paste0("data:image/", figClass, ";base64,", 
+                    base64)
+  }
+  else {
+    base64 = NULL
+  }
+  weightFactor = size * 180/max(dataOut$freq)
+  settings <- list(word = dataOut$name, freq = dataOut$freq, label = dataOut$label, 
+                   fontFamily = fontFamily, fontWeight = fontWeight, color = color, 
+                   minSize = minSize, weightFactor = weightFactor, backgroundColor = backgroundColor, 
+                   gridSize = gridSize, minRotation = minRotation, maxRotation = maxRotation, 
+                   shuffle = shuffle, rotateRatio = rotateRatio, shape = shape, 
+                   ellipticity = ellipticity, figBase64 = base64, hover = htmlwidgets::JS(hoverFunction))
+  chart = htmlwidgets::createWidget("wordcloud2", settings, 
+                                    width = widgetsize[1], height = widgetsize[2], sizingPolicy = htmlwidgets::sizingPolicy(viewer.padding = 0, 
+                                                                                                                            browser.padding = 0, browser.fill = TRUE))
+  htmlwidgets::onRender(chart, "function(el,x){\n                        console.log(123);\n                        if(!iii){\n                          window.location.reload();\n                          iii = False;\n\n                        }\n  }")
+}
+
+frequencia_palavras_2 <- frequencia_palavras%>%
+  mutate(
+    label = n,
+    n =log(n+1)
+  ) %>% 
+  select(-c(3:4))
+plot_nuvem_pandemica <- wordcloud3(
   data = frequencia_palavras_2,
   size = 0.6,          
   fontWeight = 'bold',  
-  color='random-light', 
-  backgroundColor="dark",
+  color= colorRampPalette(c("#c07068", "#ffffff"))(254), 
+  backgroundColor="#2d2f45",
   shape="circle"
 )
-
-### Bala ----
-frequencia_palavras_2 = frequencia_palavras%>%
-  mutate(n=log(n+1))
-wordcloud2(
-  data = frequencia_palavras_2,
-  size = 0.8,          
-  fontWeight = 'bold',  
-  color='random-light', 
-  backgroundColor="dark",
-  figPath = "Avaliacao_6/media/candy.png"
-)
+plot_nuvem_pandemica
+# htmlwidgets::saveWidget(plot_nuvem_pandemica, "Avaliacao_6/media/plot_nuvem_pandemica.html", selfcontained = TRUE)
