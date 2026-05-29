d7df7dedc624901f3ba2e8b84f977707fe18397a
CaioVaos <salvianocaio@gmail.com>
2026-05-28 22:45:46 -0300

    Update AV9

--- Avaliacao_9/analysis/mapas.R
+++ Avaliacao_9/analysis/mapas.R
@@ -8,7 +8,6 @@ library(dplyr)      # Manipulação de bases de dados
 # library(ggsflabel)  # Criação de Labels que se repelem
 library(ggspatial)  # Rosa dos ventos e escala
 library(sf)         # Leitura de shapefiles fora do geobr
-library(ggiraph)    # Mapas interativos
 
 # Sys.unsetenv("GITHUB_PAT")
 remotes::install_github("ipeaGIT/geobr", subdir = "r-package")
@@ -16,8 +15,13 @@ remotes::install_github("ipeaGIT/geobr", subdir = "r-package")
 devtools::install_github("yutannihilation/ggsflabel")
 
 # read_health_region -----------------------------------------------------------
+args(read_health_region)
 
 Dados <- read_health_region(year = 2025,
-                            geometry_level = "micro")
+                            geometry_level = "micro",
+                            simplified = T)
 Dados <- Dados[!st_is_empty(Dados),]
 ggplot(Dados)+geom_sf()
+
+# read_indigenous_land -----------------------------------------------------------
+args(read_indigenous_land)
